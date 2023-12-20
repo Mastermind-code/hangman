ca988a1a46df5e34cbc03d76c7fad7d2d18697c3
Adebowale Adam <adebowaleadam0@gmail.com>
2023-12-21 00:49:45 +0100

    completed html and css

---
+++ Scripts/index.js
@@ -0,0 +1,13 @@
+// const wordDisplay = document.querySelector(".word-display");
+// const keyboardDiv = document.querySelector(".keyboard");
+
+// const getRandomWord = () => {
+//   const { word, hint } = wordList[Math.floor(math.random) * wordList.length];
+//   document.querySelector(".hint-text b").innerText = hint;
+//   wordDisplay.innerHTML = word.split("").map(()=>  )
+// };
+// for (let i = 97; i < 122; i++) {
+//   const button = document.createElement("button");
+//   button.innerText = String.fromCharCode(i);
+//   keyboardDiv.appendChild(button);
+// }
